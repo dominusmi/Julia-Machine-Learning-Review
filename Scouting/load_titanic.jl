39ebf4a993ff0edd44eec325196ca4146a78ae01
harveydevereux <harveydevereux@googlemail.com>
2018-03-21 11:51:06 +0000

    points to resources

--- Scouting/load_titanic.jl
+++ Scouting/load_titanic.jl
@@ -29,7 +29,7 @@ end
 
 function load(as_table = false)
     # Load dataset
-    titanic = loadtable("titanic_train.csv")
+    titanic = loadtable("resources/titanic_train.csv")
     # remove any rows with NA values
     titanic_clean = dropna(titanic)
 
