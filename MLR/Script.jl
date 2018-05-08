dddb1ddbd3a880274305f9620248d84dd97a3b85
Unknown <barp.edoardo@gmail.com>
2018-05-08 11:46:20 +0100

    Visualisation of model working

--- MLR/Script.jl
+++ MLR/Script.jl
@@ -6,7 +6,7 @@ include("Tuning.jl")
 # include("decisiontree_wrapper.jl")
 # data = FakedataClassif(1000,3)
 #
-# task = Task(task_type="classification", target=4, data=data)
+# task = Task(task_type="classification", targets=[4], data=data)
 # lrn = Learner("forest", Dict("nsubfeatures"=>2, "ntrees"=>10))
 #
 # modelᵧ = learnᵧ(lrn, task, data)
@@ -17,7 +17,7 @@ include("Tuning.jl")
 # include("multivariate_wrapper.jl")
 # data = Fakedata(1000,4)
 #
-# task = Task(task_type="regression", target=[3], data=data)
+# task = Task(task_type="regression", targets=[3], data=data)
 # lrn = Learner("multivariate", Dict("regType"=>"llsq"))
 #
 # modelᵧ = learnᵧ(lrn, task, data)
@@ -27,31 +27,33 @@ include("Tuning.jl")
 
 ## Example regression using GLM with penalty and λ tuning
 
-# include("glm_wrapper.jl")
-# ps = ParametersSet([
-#     ContinuousParameter(
-#         name = "cost",
-#         lower = -4,
-#         upper = 1,
-#         transform = x->10^x
-#     ),
-#     DiscreteParameter(
-#         name = "penalty",
-#         values = [L2Penalty(), L1Penalty()]
-#     )
-# ])
-#
-# data = Fakedata(1000,3)
-#
-# task = Task(task_type="classification", target=4, data=data)
-# lrn = Learner("glm")
-#
-# tune(learner=lrn, task=task, data=data, parameters_set=ps,
-#     measure=mean_squared_error)
+include("glm_wrapper.jl")
+ps = ParametersSet([
+    ContinuousParameter(
+        name = "cost",
+        lower = -4,
+        upper = 1,
+        transform = x->10^x
+    ),
+    DiscreteParameter(
+        name = "penalty",
+        values = [L2Penalty(), L1Penalty()]
+    )
+])
 
+data = Fakedata(1000,3)
 
+task = Task(task_type="classification", targets=[4], data=data)
+lrn = Learner("glm")
 
+storage = MLRStorage()
 
+tune(learner=lrn, task=task, data=data, parameters_set=ps,
+    measure=mean_squared_error, storage=storage)
+
+include("Visualisation.jl")
+
+plot_storage(storage)
 ## Example classification using SVM with type and cost tuning
 
 # include("libsvm_wrapper.jl")
@@ -80,7 +82,7 @@ include("Tuning.jl")
 #
 # data = FakedataClassif(1000,3)
 #
-# task = Task(task_type="classification", target=4, data=data)
+# task = Task(task_type="classification", targets=[4], data=data)
 # lrn = Learner("libsvm")
 #
 # tune(learner=lrn, task=task, data=data, parameters_set=ps,
--- MLR/Visualisation.jl
+++ MLR/Visualisation.jl
@@ -1,31 +1,34 @@
 using Plots
 plotlyjs()
 
-
-x = [1,2,3]
-y = [2,3,1]
-
-text = ["ergerg", "ergerg", "ggre"]
-
-plot(x,y, hover=(text))
+# x = 1:3
+# y = 1:3
+# text = [["ergerg", "a"], ["ergerg", "a"], "ggre"]
+#
+# plot(x,y, hover=(text))
 
 
 function plot_storage(storage::MLRStorage)
     models = Set(storage.models)
     markers = []
+    measures = []
 
     for model in models
         indeces = []
-        for i, p_models in enumerate(models)
+        for (i, p_models) in enumerate(storage.models)
             if p_models == model
-                push!(indeces, model)
+                push!(indeces, i)
             end
         end
-        y = storage.averageCV[indeces]
-        for key, value in storage.params[indeces]
-            push!(markers, "$key: $value")
+        measures = storage.averageCV[indeces]
+        for dict in storage.parameters[indeces]
+            _marker = ""
+            for (key, value) in dict
+                _marker = _marker * "$key: $value, "
+            end
+            push!(markers, _marker)
         end
     end
 
-    plot()
+    plot(measures, hover=markers, label=models)
 end
