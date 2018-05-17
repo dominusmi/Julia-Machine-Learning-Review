a425e48b25306cca2c690049782a40a53291d830
annika-stechemesser <a.stechemesser@warwick.ac.uk>
2018-05-17 18:31:23 +0100

    added inline docs

--- MondrianForest/Mondrian_extention.jl
+++ MondrianForest/Mondrian_extention.jl
@@ -1,24 +1,66 @@
-#Go at implementing Alg 3
+include("Mondrian_extention_utils.jl");
+"""
+`function Extend_Mondrian_Tree!(T::Mondrian_Tree,λ::Float64,X::Array{Float64} where N,Y::Int64) `
 
-function Extend_Mondrian_Tree!(T,λ,D) #Mondrian Tree, λ, D -> new training instance as a tuple
+This function extends an already existing Mondrian Tree by ONE new datapoint that gets incorperated in the tree. 
+
+`Input`: Mondrian Tree T (abstract type Mondrian_Tree), Lifetime parameter λ (Float64), 1dim Array of Features Array X (Array of Float64), classlabel Y (Int64)
+
+`Output`: Mondrian Tree
+
+`Files needed to run this function`: "Mondrian_Forest_Classifier.jl", "Mondrian_extention.jl"
+
+This function calls the function Extend_Mondrian_Block.
+
+The usage of the function "expand!" is recommended to expand Mondrian Trees as it has a nicer user interface.
+
+"""
+## Algorithm 3 in the Paper "Mondrian Forests: Efficient Online Random Forests"
+function Extend_Mondrian_Tree!(T::Mondrian_Tree,λ::Float64,X::Array{Float64},Y::Int64) 
     ϵ=get(T.root)
-    Extend_Mondrian_Block!(T,λ,ϵ,D) #Input: Mondrian Tree, lifetime, rootnode, new training instance
+    Extend_Mondrian_Block!(T,λ,ϵ,X,Y)  
     return T
 end
 
-function Extend_Mondrian_Block!(T,λ,j,D) # Input: Mondrian Tree, lifetime, node, new training instance as a tuple
-    E = rand(Exponential(1/Extended_dimension(get(j.Θ),D[1])))
-    if j.node_type[3]==true #if its a root
+
+"""
+`function Extend_Mondrian_Block!(T::Mondrian_Tree,λ::Float64,j::Mondrian_Node,X::Array{Float64},Y::Int64)`
+
+This function extends a Mondrian Block to incorporate ONE new datapoint. 
+
+`Input`: Mondrian Tree T (abstract type Mondrian_Tree), Lifetime parameter λ (Float64), Mondrian Node j (abstract type Mondrian_Node), 1dim Array of Features Array X (Array of Float64), classlabel Y (Int64)
+
+`Output`: Modified nodes in the input tree
+
+`Files needed to run this function`: Mondrian_Forest_Classifier.jl", "Mondrian_extention.jl"
+
+This function calls the functions 
+
+- Extended_dimension()
+
+- sample_extended_split_dimension
+
+- Sample_Mondrian_Block!
+
+- update_intervals
+
+
+"""
+
+## Algorithm 4 in the Paper "Mondrian Forests: Efficient Online Random Forests"
+function Extend_Mondrian_Block!(T::Mondrian_Tree,λ::Float64,j::Mondrian_Node,X::Array{Float64},Y::Int64)
+    
+    E = rand(Exponential(1/Extended_dimension(get(j.Θ),X)))  #sample value E
+    if j.node_type[3]==true                                  # check if the node we're looking at is the root (if yes the split time is assumed to be 0)
         τₚ = 0
     else
-        τₚ = (get(j.parent)).τ
+        τₚ = (get(j.parent)).τ                               # if it's not the root get the split time of the node above j
     end
-    # if split occured in time
-    if τₚ + E < j.τ
-        d,x= sample_extended_split_dimension(get(j.Θ),D[1])
-        Θ = update_intervals(get(j.Θ),D)
-        if j.node_type[3]==true  ## check if we replace the root
-            j_wave=Mondrian_Node(E,[true,false,false])
+    if τₚ + E < j.τ                                          # check if our split happens in time
+        d,x= sample_extended_split_dimension(get(j.Θ),X)     # sample new split dimension / split direction
+        Θ = update_intervals(get(j.Θ),X)                     # get the boxes for the new node
+        if j.node_type[3]==true                              # check if we replace the root
+            j_wave=Mondrian_Node(E,[true,false,false])       # replace the root by the new node j_wave
 	    j_wave.δ = d
         j_wave.ζ = x
         j_wave.Θ = Θ
@@ -26,7 +68,7 @@ function Extend_Mondrian_Block!(T,λ,j,D) # Input: Mondrian Tree, lifetime, node
         j_wave.c = zeros(size(get(j.c)))
         j_wave.Gₚ = zeros(size(get(j.Gₚ)))
         else
-            j_wave=Mondrian_Node(get(j.parent).τ+E,[true,false,false])
+            j_wave=Mondrian_Node(get(j.parent).τ+E,[true,false,false])  #if we don't replace the root, introcue a new node j_wave, parent to j
 	    j_wave.parent = j.parent
 	    j_wave.δ = d
         j_wave.ζ = x
@@ -34,7 +76,7 @@ function Extend_Mondrian_Block!(T,λ,j,D) # Input: Mondrian Tree, lifetime, node
         j_wave.tab = get(j.parent).tab
         j_wave.c = get(j.parent).c
         j_wave.Gₚ = get(j.parent).Gₚ
-            if j == get(j.parent).left
+            if j == get(j.parent).left             # check if j was left or right child of j_parent
                 get(j.parent).left = j_wave
             else
                 get(j.parent).right = j_wave
@@ -48,40 +90,66 @@ function Extend_Mondrian_Block!(T,λ,j,D) # Input: Mondrian Tree, lifetime, node
         j_prime.c = zeros(size(j_wave.tab))
         j_prime.Gₚ=zeros(size(j_wave.c,1))
         
-        if D[1][d] > x
+        if X[d] > x     # check where the new datapoint lies
             j_wave.left = j
             j_wave.right = j_prime 
-                if get(j.Θ).Intervals[d,2]> x    #adapt box of j
-                    get(j.Θ).Intervals[d,2] = x
+                if get(j.Θ).Intervals[d,2]> x    
+                    get(j.Θ).Intervals[d,2] = x  #adapt box of j according to new split
                 end
             j_prime.Θ = j_wave.Θ
             get(j_wave.Θ).Intervals[d,1] = x
-            A=zeros(1,length(D[1]))
-            A[:,:]=D[1]
-            Sample_Mondrian_Block!(j_prime,get(j_prime.Θ),λ,T,A,[D[2]])
+            A=zeros(1,length(X))
+            A[:,:]=X
+            Sample_Mondrian_Block!(j_prime,get(j_prime.Θ),λ,T,A,[Y])  #sample a mondrian block at the node whose associated boxes contain the new datapoint
         else
             j_wave.left = j_prime
             j_wave.right = j
-            if get(j.Θ).Intervals[d,1]< x
-                get(j.Θ).Intervals[d,1]= x
+            if get(j.Θ).Intervals[d,1]< x 
+                get(j.Θ).Intervals[d,1]= x      #adapt box of j according to new split
             end
             j_prime.Θ = j_wave.Θ
             get(j_wave.Θ).Intervals[d,2] = x
-            A=zeros(1,length(D[1]))
-            A[:,:]=D[1]
-            Sample_Mondrian_Block!(j_prime,get(j_prime.Θ),λ,T,A,[D[2]])
+            A=zeros(1,length(X))
+            A[:,:]=X
+            Sample_Mondrian_Block!(j_prime,get(j_prime.Θ),λ,T,A,[Y])  #sample a mondrian block at the node whose associated boxes contain the new datapoint
         end
 
 
-    else
-        Θ = update_intervals(get(j.Θ),D)
+        else                                    # if the split didn't occur in time
+        Θ = update_intervals(get(j.Θ),X)        # update the boxes of j
         j.Θ=Θ
-        if j.node_type != [false,true,false]
-            if D[1][get(j.δ)] < get(j.ζ)
-                Extend_Mondrian_Block!(T,λ,get(j.left),D)
+        if j.node_type != [false,true,false]    # check if j is a leaf
+            if X[get(j.δ)] < get(j.ζ)           # if the new datapoint is in the boxes associated with the left child of j -> Extend towards the left child, else the right
+                Extend_Mondrian_Block!(T,λ,get(j.left),X,Y)
             else
-                Extend_Mondrian_Block!(T,λ,get(j.right),D)
+                Extend_Mondrian_Block!(T,λ,get(j.right),X,Y)
             end
         end
     end
+end
+
+"""
+`function expand!(T::Mondrian_Tree,X::Array{Float64,N} where N,Y::Array{Int64},λ::Float64)`
+
+This function expands an already sampled Mondrian Tree by a desired number of datapoints. 
+
+`Input`: Mondrian Tree T (abstract type Mondrian_Tree), array of features X (Array of Float64), array of class labels (1dim of Float 64), Lifetime parameter λ (Float 64)
+
+Each row in the array X represents one set of features, the corresponding row in Y represents the class label. 
+
+`Output`: Mondrian Tree with incoporated new datapoints
+
+`Files needed to run this function`: Mondrian_Forest_Classifier.jl", "Mondrian_extention.jl"
+
+This function calls the function Extend_Mondrian_Tree. 
+"""
+
+function expand!(T::Mondrian_Tree,X::Array{Float64,N} where N,Y::Array{Int64},λ::Float64)    
+    
+    # puts the extention in a nice framework, allows to extend by multiple datapoints
+    
+    for i=1:length(X[:,1])
+        T=Extend_Mondrian_Tree!(T,λ,X[i,:],Y[i]);
+    end
+    return T    
 end
--- MondrianForest/Mondrian_extention_utils.jl
+++ MondrianForest/Mondrian_extention_utils.jl
@@ -1,70 +1,82 @@
-#utils for Alg 3 and 4 (need to go in a file)
+#Utils for extending Mondrian Trees 
 
-# ∑ᵢ₌₁ᴰmax(lⱼ-x,0)+max(x-uⱼ,0)
-function Extended_dimension(Θ::Axis_Aligned_Box,datapoint) #input: boxes, datapoint (no label)
+"""
+`function Extended_dimension(Θ::Axis_Aligned_Box,X::Array{Float64})`
+
+This function samples the rate ∑ᵢ₌₁ᴰmax(lⱼ-x,0)+max(x-uⱼ,0) with which we draw the parameter E from the exponential distribution in the function Extend_Mondrian_Block!. 
+
+`Input`:  Axis Aligned Box Θ (Abstract type Axis_Aligned_Box), One dimensional array of features X (Array of Float64)
+
+`Output`: Sampling rate s
+
+`Files needed to run this function`: "Mondrian_extention.jl"
+
+This function is one of the utility functions to run "Extend_Mondrian_Block!".
+
+
+"""
+
+function Extended_dimension(Θ::Axis_Aligned_Box,X::Array{Float64}) 
+    # rate = ∑ᵢ₌₁ᴰmax(lⱼ-x,0)+max(x-uⱼ,0)
+    
     s = 0
     for i in 1:Θ.D
-        s += max(Θ.Intervals[i,1]-datapoint[i],0)+max(datapoint[i]-Θ.Intervals[i,2],0)
+        s += max(Θ.Intervals[i,1]-X[i],0)+max(X[i]-Θ.Intervals[i,2],0)  #compute the rate
     end
     return s
 end
 
-function sample_extended_split_dimension(Θ,datapoint) #input: boxes, datapoint (no label)
+"""
+`function sample_extended_split_dimension(Θ::Axis_Aligned_Box,X::Array{Float64})`
+
+This function samples the split dimension and direction associated to the new node j_wave in the function "Extend_Mondrian_Block!"
+
+`Input`: Axis Aligned Box Θ (abstract type Axis_Aligned_Box), One dimensional array of features X (Array of Float64)
+
+`Output`: Split dimension d, split direction s
+
+`Files needed to run this function`: "Mondrian_extention.jl"
+
+This function is one of the utility functions to run "Extend_Mondrian_Block!".
+"""
+
+function sample_extended_split_dimension(Θ::Axis_Aligned_Box,X::Array{Float64}) 
     p_k = zeros(Θ.D)
     for i in 1:Θ.D
-        p_k[i] = max(Θ.Intervals[i,1]-datapoint[i],0)+max(datapoint[i]-Θ.Intervals[i,2],0)
+        p_k[i] = max(Θ.Intervals[i,1]-X[i],0)+max(X[i]-Θ.Intervals[i,2],0)  #compute max(lⱼ-x,0)+max(x-uⱼ,0)
     end
-    p_k = p_k ./ Extended_dimension(Θ,datapoint)  #create the probability array
-    d = rand(Categorical(p_k))        # sample from it
+    p_k = p_k ./ Extended_dimension(Θ,X)                                #create the probability array
+    d = rand(Categorical(p_k))                                          # sample from it (d with probability proportional to max(lⱼ-x,0)+max(x-uⱼ,0) )
 
-    if datapoint[d] > Θ.Intervals[d,2]
-    x = rand(Uniform(Θ.Intervals[d,2][1],datapoint[d]))#sample the split loc. uniformly from that interval
+    if X[d] > Θ.Intervals[d,2]  # choice of interval dependent on where the new datapoint is                                    
+    x = rand(Uniform(Θ.Intervals[d,2][1],X[d]))  #sample the split loc. uniformly from that interval
     else
-    x = rand(Uniform(datapoint[d],Θ.Intervals[d,1][1]))
+    x = rand(Uniform(X[d],Θ.Intervals[d,1][1])) #sample the split loc. uniformly from that interval
     end
-    return d,x
+    return d,x   #return split dim., split loc. 
 end
 
-function Mondrian_Node(parent::Nullable{Mondrian_Node},τ::Float64,node_type::Array{Bool,1},δ::Nullable{Int},ζ::Nullable{Float64},Θ::Nullable{Axis_Aligned_Box},tab::Array{Int}, c::Array{Int}, Gₚ::Array{Float64})  #constructor used in Alg4 (Extended_Mond)
-    N = Mondrian_Node(parent,
-                      left::Nullable{Mondrian_Node},
-                      right::Nullable{Mondrian_Node},
-                      τ,node_type,
-                      δ,
-                      ζ,
-                      Θ,
-                      tab,
-                      c,
-                      Gₚ,
-                      Array{Int}())
-    return N
-end
+"""
+`function update_intervals(Θ::Axis_Aligned_Box,X::Array{Float64})`
 
+This function computes the boxes associated to the new node j_wave. 
 
-function Mondrian_Node(τ::Float64,node_type::Array{Bool,1},δ::Nullable{Int},ζ::Nullable{Float64},Θ::Nullable{Axis_Aligned_Box},tab::Array{Int},c::Array{Int},Gₚ::Array{Float64})  #constructor used in Alg4
-    N = Mondrian_Node(Nullable{Mondrian_Node}(),
-                      left::Nullable{Mondrian_Node},
-                      right::Nullable{Mondrian_Node},
-                      left,
-                      right,
-                      τ,node_type,
-                      δ,
-                      ζ,
-                      Θ,
-                      tab,
-                      c,
-                      Gₚ,
-                      Array{Int}())
-    return N
-end
+`Input`: Axis Aligned Box Θ (abstract type Axis_Aligned_Box), One dimensional array of features X (Array of Float64)
 
-function update_intervals(Θ::Axis_Aligned_Box,D) #take θ and Datapoint as a tuple
-    Intervals = Θ.Intervals
-   for i=1:length(Θ.Intervals[:,1])
-       l=min(Intervals[i,1],D[1][i])
-       u=max(Intervals[i,2],D[1][i])
+`Output`: Axis Aligned Box Θ (updated Intervals)
+
+`Files needed to run this function`: "Mondrian_extention.jl"
+
+This function is one of the utility functions to run "Extend_Mondrian_Block!".
+
+"""
+function update_intervals(Θ::Axis_Aligned_Box,X::Array{Float64})
+    Intervals = Θ.Intervals                         # extract the intervals from Θ
+   for i=1:length(Θ.Intervals[:,1])                 # update the intervals successively 
+       l=min(Intervals[i,1],X[i])
+       u=max(Intervals[i,2],X[i])
        Intervals[i,:]=[l,u]
     end
-    Θ = Axis_Aligned_Box(Intervals,Θ.D)
+    Θ = Axis_Aligned_Box(Intervals,Θ.D)             # return updated intervals
     return Θ
 end
