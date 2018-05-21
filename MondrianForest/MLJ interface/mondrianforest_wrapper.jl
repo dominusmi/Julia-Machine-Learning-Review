65d1c8c5e31757f438ef29b03b9766b468243e3a
Harvey Devereux <harveydevereux@googlemail.com>
2018-05-21 12:04:01 +0100

    updated MF-MLJ wrapper

--- MondrianForest/MLJ interface/mondrianforest_wrapper.jl
+++ MondrianForest/MLJ interface/mondrianforest_wrapper.jl
@@ -1,8 +1,9 @@
-function makeMondrianForest(learner::Learner, task::Task, data)
+function makeMondrianForest(learner::ModelLearner, task::Task)
     parameters = []
     possible_names = ["n_trees"]
     possible_parameters = Dict(
-        "n_trees" => Integer
+        "n_trees" => Integer,
+        "λ" => Float64
     )
     for (i, (name,value)) in enumerate(learner.parameters)
         if possible_names[i] == name
@@ -14,22 +15,24 @@ function makeMondrianForest(learner::Learner, task::Task, data)
     MF=Mondrian_Forest_Classifier()
     MLRModel(MF, parameters, inplace=false)
 end
-function learnᵧ(modelᵧ::MLRModel{<:Mondrian_Forest_Classifier};
-                learner=nothing::Learner,
-                data=nothing::Matrix{Real},
-                task=nothing::Task)
+function learnᵧ(modelᵧ::MLRModel{<:Mondrian_Forest_Classifier},
+                learner::Learner,
+                task::Task)
     train = data[:,task.features]
-    target = convert(Array{Int64,1},data[:,task.targets[1]])
+    target = data[:,task.targets[1]]
     MF = Mondrian_Forest_Classifier(modelᵧ.parameters...)
-    train!(MF, train, target)
+    train!(MF, train, convert(Array{Int64},target))
     MLRModel(MF, modelᵧ.parameters)
 end
 
-function predictᵧ(modelᵧ::MLRModel{<:Mondrian_Forest_Classifier};
-                    data_features=nothing::Matrix, task=nothing::Task)
-    preds = predict!(modelᵧ.model, data_features)
-    ## TODO get predict_proba! working for mondrian forest and tree
-    # println(probs)
-    # preds = [p>0.5?1:0 for p in probs]
+function predictᵧ(modelᵧ::MLRModel{<:Mondrian_Forest_Classifier},
+                    data_features, task::Task)
+    # required otherwise predictᵧ returns 0,0 probs!    
+    println()
+    probs = predict_proba!(modelᵧ.model, data_features)
+    preds = zeros(size(probs,1))
+    for i in 1:size(probs,1)
+        preds[i] = indmax(probs[i])
+    end
     preds, probs
 end
