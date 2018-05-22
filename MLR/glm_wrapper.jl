b09fa9dce963066e89c9c3eff296032d204e28f3
Unknown <barp.edoardo@gmail.com>
2018-05-22 11:50:52 +0100

    Quick fix GLM

--- MLR/glm_wrapper.jl
+++ MLR/glm_wrapper.jl
@@ -67,17 +67,17 @@ function makeGlm(learner::Learner, task::Task)
         model = SModel(task.data[:, task.features], task.data[:, task.targets])
     else
         parameters = []
-        if get(learner.parameters, "λ", false) !== false
+        if get(learner.parameters, :λ, false) !== false
             # Add λ
             push!(parameters, get_λ(learner.parameters, task))
         end
-        if get(learner.parameters, "penalty", false) !== false
+        if get(learner.parameters, :penalty, false) !== false
             # Add penalty
-            push!(parameters, learner.parameters["penalty"])
+            push!(parameters, learner.parameters[:penalty])
         end
-        if get(learner.parameters, "loss", false) !== false
+        if get(learner.parameters, :loss, false) !== false
             # Add penalty
-            push!(parameters, learner.parameters["loss"])
+            push!(parameters, learner.parameters[:loss])
         end
         model = SModel(task.data[:, task.features], task.data[:, task.targets[1]], parameters...)
     end
@@ -90,7 +90,7 @@ function get_λ(parameters, task::Task)
         lambda = fill(0.0, task.features)
     elseif typeof(parameters["λ"]) <: Real
         lambda = fill(parameters["λ"], length(task.features) )
-    elseif typeof(parameters["λ"]["type"]) <: Vector{Real}
+    elseif typeof(parameters["λ"]) <: Vector{AbstractFloat}
         lambda = copy(parameters["λ"])
     end
     lambda
